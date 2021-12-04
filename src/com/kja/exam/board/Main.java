914572a803cdb7237cdc90c7763e013576479307
jiin-coder <fpahstiq123@naver.com>
2021-12-04 20:13:14 +0900

    exit가 입력될 때까지 명령어 계속 입력받기

--- src/com/kja/exam/board/Main.java
+++ src/com/kja/exam/board/Main.java
@@ -3,17 +3,22 @@ package com.kja.exam.board;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
 
-        System.out.printf("명령) ");
-        String cmd = sc.nextLine();
-        System.out.printf("입력된 명령어 : %s\n", cmd);
+        while (true){
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine();
 
+            if (cmd.equals("exit")){
+                break;
+            }
+
+            System.out.printf("입력된 명령어 : %s\n", cmd);
+        }
         System.out.println("== 프로그램 종료 ==");
 
         sc.close();
