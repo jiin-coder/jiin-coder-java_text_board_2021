f21ae02d79e71282bf020638225ea4fa182835e9
jiin-coder <fpahstiq123@naver.com>
2021-12-04 20:33:15 +0900

    /usr/article/detail 명령으로 최근 게시물 보기

--- src/com/kja/exam/board/Main.java
+++ src/com/kja/exam/board/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("== 프로그램 시작 ==");
 
         int articlesLastId = 0;
+        Article lastArticle = null;
 
         while (true) {
             System.out.printf("명령) ");
@@ -17,6 +18,21 @@ public class Main {
 
             if (cmd.equals("exit")) {
                 break;
+
+            } else if (cmd.equals("/usr/article/detail")) {
+
+                if (lastArticle == null) {
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
+                Article article = lastArticle;
+
+                System.out.println("- 게시물 상세내용 -");
+                System.out.printf("번호 : %d\n", article.id);
+                System.out.printf("제목 : %s\n", article.title);
+                System.out.printf("내용 : %s\n", article.body);
+
             } else if (cmd.equals("/usr/article/write")) {
                 System.out.println("- 게시물 등록 -");
                 System.out.printf("제목 : ");
@@ -28,13 +44,14 @@ public class Main {
                 articlesLastId = id;
 
                 Article article = new Article(id, title, body);
+                lastArticle = article;
                 System.out.println("생성된 게시물 객체 : " + article);
 
                 System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+
             } else {
                 System.out.printf("입력된 명령어 : %s\n", cmd);
             }
-
         }
         System.out.println("== 프로그램 종료 ==");
 
@@ -42,19 +59,20 @@ public class Main {
     }
 }
 
+
 class Article {
     int id;
     String title;
     String body;
 
-    Article(int id, String title, String body){
+    Article(int id, String title, String body) {
         this.id = id;
         this.title = title;
         this.body = body;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
     }
 }
