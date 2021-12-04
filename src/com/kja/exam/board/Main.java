33486a08f762013ab1e2bacbf24d21e953fe9202
jiin-coder <fpahstiq123@naver.com>
2021-12-04 20:35:50 +0900

    /usr/article/list 로 입력하면 게시물리스트 출력, 시작

--- src/com/kja/exam/board/Main.java
+++ src/com/kja/exam/board/Main.java
@@ -1,5 +1,6 @@
 package com.kja.exam.board;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,12 +13,30 @@ public class Main {
         int articlesLastId = 0;
         Article lastArticle = null;
 
+        ArrayList<Article> articles = new ArrayList<Article>();
+
+        // 테스트 데이터 3개 등록, 시작
+        articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
+        // 테스트 데이터 3개 등록, 끝
+
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
 
             if (cmd.equals("exit")) {
                 break;
+
+            } else if (cmd.equals("/usr/article/list")) {
+                System.out.println("- 게시물 리스트 -");
+                System.out.println("--------------------");
+                System.out.println("번호 / 제목");
+                System.out.println("--------------------");
+
+                for (Article article : articles) {
+                    System.out.printf("%d / %s\n", article.id, article.title);
+                }
 
             } else if (cmd.equals("/usr/article/detail")) {
 
