a33671b8e1197644331e792e3cac50cd9fb17c20
jiin-coder <fpahstiq123@naver.com>
2021-12-13 16:48:13 +0900

    복잡한 명령어를 처리하기 위해, Rq 도입

--- src/com/kja/exam/board/Main.java
+++ src/com/kja/exam/board/Main.java
@@ -1,8 +1,6 @@
 package com.kja.exam.board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static void makeTestData(List<Article> articles) {
@@ -32,9 +30,11 @@ public class Main {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("exit")) {
-                break;
+            Rq rq = new Rq(cmd);
 
+
+            if (rq.getUrlPath().equals("exit")) {
+                break;
             } else if (cmd.equals("/usr/article/list")) {
                 System.out.println("- 게시물 리스트 -");
                 System.out.println("--------------------");
@@ -60,7 +60,7 @@ public class Main {
                 System.out.printf("제목 : %s\n", article.title);
                 System.out.printf("내용 : %s\n", article.body);
 
-            } else if (cmd.equals("/usr/article/write")) {
+            } else if (rq.getUrlPath().equals("/usr/article/write")) {
                 System.out.println("- 게시물 등록 -");
                 System.out.printf("제목 : ");
                 String title = sc.nextLine();
@@ -102,5 +102,53 @@ class Article {
     @Override
     public String toString() {
         return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
+    }
+}
+
+class Rq {
+    private String url;
+    private String urlPath;
+    private Map<String, String> params;
+
+
+    Rq(String url) {
+        this.url = url;
+        urlPath = Util.getUrlPathFromUrl(this.url);
+        params = Util.getParamsFromUrl(this.url);
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public String getUrlPath() {
+        return urlPath;
+    }
+}
+
+
+class Util {
+    static Map<String, String> getParamsFromUrl(String url) {
+        Map<String, String> params = new HashMap<>();
+        String[] urlBits = url.split("\\?", 2);
+
+        if (urlBits.length == 1) {
+            return params;
+        }
+
+        String queryStr = urlBits[1];
+        for (String bit : queryStr.split("&")) {
+            String[] bits = bit.split("=", 2);
+            if (bits.length == 1) {
+                continue;
+            }
+            params.put(bits[0], bits[1]);
+        }
+
+        return params;
+    }
+
+    static String getUrlPathFromUrl(String url) {
+        return url.split("\\?", 2)[0];
     }
 }
